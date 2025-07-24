1cad02968ca18777a535887dcb99c4314bf01e46
Luan CT <caothanhluan91298@gmail.com>
2025-07-24 11:45:07 +0700

    fix: sau lai file test cua app spec

--- apps/demo/src/app/app.spec.ts
+++ apps/demo/src/app/app.spec.ts
@@ -1,18 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])], // Cần cung cấp router để tránh lỗi khi test router-outlet
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome demo');
+    // Test nếu router-outlet tồn tại
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
